14ad515493e7c2f0420cd622f47a0b076c0eeb22
progamler <github@guerilla-itde>
2013-05-21 15:32:57 +0300

    Update rc-controll.py
    some clenup

--- rc-controll.py
+++ rc-controll.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python2
 #
-# joystick-servo.py
-#
-# created 19 December 2007
-# copyleft 2007 Brian D. Wendt
-# http://principialabs.com/
-#
-# code adapted from:
-# http://svn.lee.org/swarm/trunk/mothernode/python/multijoy.py
-#
+# 
 # NOTE: This script requires the following Python modules:
-#  pyserial - http://pyserial.sourceforge.net/
 #  pygame   - http://www.pygame.org/
 # Win32 users may also need:
 #  pywin32  - http://sourceforge.net/projects/pywin32/
@@ -21,15 +12,8 @@ import socket
 # allow multiple joysticks
 joy = []
 
-# Arduino USB port address (try "COM5" on Win32)
-usbport = "/dev/ttyUSB0"
-
-# define usb serial connection to Arduino
-#ser = serial.Serial(usbport, 9600)
 HOST = '195.160.169.42'    # The remote host
 PORT = 50007              # The same port as used by the server
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#.connect((HOST, PORT))
 
 value = 150
 # handle joystick event
@@ -54,23 +38,17 @@ def handleJoyEvent(e):
             # uncomment to debug
             #output(str, e.dict['joy'])
 
-            ## Arduino joystick-servo hack
+            ## Send Servo Value to boat
             if (axis == "X"):
                 pos = e.dict['value']
-                ## convert joystick position to servo increment, 0-180
+               
                 print(str)
             elif (axis == "Throttle"):
                 print(str)
+                ## convert joystick position to servo increment
                 x = (225*e.dict['value'])+375
-                #x = x + 375
                 print(x)
-            #        s.sendall('-')
-                ## convert position to ASCII character
-                #servoPosition = chr(servo)
-                ## and send to Arduino over serial connection
-                #ser.write(servoPosition)
-                # uncomment to debug
-                #print servo, servoPosition
+
 		
 
     elif e.type == pygame.JOYBUTTONDOWN:
@@ -79,7 +57,7 @@ def handleJoyEvent(e):
         #output(str, e.dict['joy'])
         # Button 0 (trigger) to quit
         if (e.dict['button'] == 0):
-            #print "Bye!n"
+            print "Bye!n"
             s.close()
             quit()
     else:
